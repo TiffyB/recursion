87594668b1e35ef4cab98ffd21df294215ab4b98
TiffyB <tiffanyabarth@gmail.com>
2017-08-09 09:00:18 -0700

    StringifyJSON function is complete and meeting spec.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,48 @@
 
 // but you don't so you're going to write it from scratch:
 
+
 var stringifyJSON = function(obj) {
-  // your code goes here
+  if (Array.isArray(obj)) {
+    if (obj.length === 0) {
+      return "".concat("[]");
+    }
+  	var elements = [];
+    for (i = 0; i < obj.length; i++) {
+  		if (!Array.isArray(obj[i]) && typeof obj[i] !== "object" && typeof obj[i] !== "string") {
+  			elements.push("".concat(obj[i]));
+  		} else {
+        elements.push(stringifyJSON(obj[i]));
+  		}
+  	}
+    return "".concat("[", elements.join(","), "]");
+  }
+  if (!Array.isArray(obj) && typeof obj === "object" && obj !== null) {
+  	var properties = [];
+    if (Object.keys(obj).length === 0){
+      return "".concat("{}");
+    }
+  	for (var key in obj) {
+      if (key === "undefined") {
+        properties;
+      } else if (!Array.isArray(obj[key]) && typeof obj[key] !== "object" && typeof obj[key] !== "string" && typeof obj[key] !== "function") {
+	  		var propertyStr = "".concat("\"", key, "\"", ":", obj[key]);
+	  		properties.push(propertyStr);
+	  	} else if (typeof obj[key] === "function" || typeof obj[key] === "undefined"){
+        properties;
+      } else {
+        properties.push("".concat("\"", key, "\":", stringifyJSON(obj[key])));
+      }
+  	}
+    return "".concat("{", properties.join(","), "}");
+  }
+  
+  if (typeof obj === "string") {
+    return "".concat("\"", obj, "\"");
+  }
+  
+  return "".concat(obj);
 };
+
+
+
